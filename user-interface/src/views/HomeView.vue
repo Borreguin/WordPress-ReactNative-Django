c37b9e8605f8666f4380665cf2da4f33354b1862
RobertoSanchezFormos <roberto.sanchez@formos.com>
2022-05-01 23:55:50 -0500

    UI1: initial template for component development.

--- user-interface/src/views/HomeView.vue
+++ user-interface/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ComponentsMenu msg="Component Development Tool" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import ComponentsMenu from "@/components/ComponentsMenu.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    ComponentsMenu,
   },
 });
 </script>
