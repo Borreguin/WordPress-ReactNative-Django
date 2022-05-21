6b7f4ec0a99643e2c7dcbfdbe747e3d1fc506225
RobertoSanchezFormos <roberto.sanchez@formos.com>
2022-05-20 23:25:07 -0500

    UI2: Draft Login

---
+++ user-interface/src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Image, useBreakpointValue } from "native-base";
+import LogoImage from "../../resources/images/logo.png";
+import { bkpBigImage } from "../../styles/breackpoints";
+
+export const Logo = () => {
+  const logoSize = useBreakpointValue(bkpBigImage);
+  return <Image source={LogoImage} resizeMode={"contain"} size={logoSize} />;
+};
