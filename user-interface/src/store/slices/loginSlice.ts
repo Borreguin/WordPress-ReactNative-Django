efe9598dbdc929a92adf7e684d6e69958264e440
RobertoSanchezFormos <roberto.sanchez@formos.com>
2022-05-26 17:05:53 -0500

    UI2: draft login functions

--- user-interface/src/store/slices/loginSlice.ts
+++ user-interface/src/store/slices/loginSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { LOGIN_SLICE } from "../storeConstants";
 import i18n from "i18next";
 import { RootState } from "../store";
+import { wpJWTAPI } from "../../constants/wp-api.constants";
 
 // Define a type for the slice state
 interface LoginState {
@@ -54,3 +55,50 @@ export const { loginOnFailure, loginOnSuccess, loginRequested, loginReset } =
 export const loginSelector = (state: RootState) => state.login;
 
 export default loginSlice.reducer;
+
+// const getToken = async (userName, password) => {
+//   const body = { username: userName, password: password };
+//   const resp = await post(wpJWTAPI.token, body);
+//
+//   if (resp.status === httpCodes.ok) {
+//     const { token, user_email, user_nicename, user_display_name } = resp.data;
+//     const user = {
+//       user_email: user_email,
+//       user_nicename: user_nicename,
+//       user_display_name: user_display_name,
+//     };
+//     return {
+//       isLoggedIn: true,
+//       user: user,
+//       token: token,
+//       message: "User logged successfully",
+//     };
+//   }
+//
+//   return { isLoggedIn: false, message: resp.data?.message };
+// };
+
+// fetch all items
+export const getToken = (userName, password) => {
+  return async (dispatch) => {
+    const body = { username: userName, password: password };
+    const resp = await post(wpJWTAPI.token, body);
+    //   if (resp.status === httpCodes.ok) {
+    //     const { token, user_email, user_nicename, user_display_name } = resp.data;
+    //     const user = {
+    //       user_email: user_email,
+    //       user_nicename: user_nicename,
+    //       user_display_name: user_display_name,
+    //     };
+    //     return {
+    //       isLoggedIn: true,
+    //       user: user,
+    //       token: token,
+    //       message: "User logged successfully",
+    //     };
+    //   }
+    //
+    //   return { isLoggedIn: false, message: resp.data?.message };
+    // };
+  };
+};
