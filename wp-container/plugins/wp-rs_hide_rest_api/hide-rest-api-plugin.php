42e14935a28001859eb9ee4fc79d594b335aa5c3
RobertoSanchezFormos <roberto.sanchez@formos.com>
2022-05-28 18:40:56 -0500

    UI2: White list auth rest access so the user can log in.

--- wp-container/plugins/wp-rs_hide_rest_api/hide-rest-api-plugin.php
+++ wp-container/plugins/wp-rs_hide_rest_api/hide-rest-api-plugin.php
@@ -15,7 +15,17 @@ add_filter( 'rest_authentication_errors', function( $result ) {
     return $result;
   }
   if ( ! is_user_logged_in() ) {
-    return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );
+    global $wp;
+    // To obtain url of the requested resource, this allows access for not logged in users:
+    // To obtain url of the requested resource, this allows access for not logged in users:
+    $request = $wp->request;
+    $route = empty( $request) ? $wp->query_vars['rest_route']: $request;
+    switch ($route) {
+      case '/jwt-login/v1/auth':
+        return $result;
+      default:
+        return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );
+    }
   }
   if ( ! current_user_can( 'administrator' ) ) {
     return new WP_Error( 'rest_not_admin', 'You are not an administrator.', array( 'status' => 401 ) );
