766a13b8fe2e51c4af78f6d81a484a122a5d1478
RobertoSanchezFormos <roberto.sanchez@formos.com>
2022-06-10 23:03:51 -0500

    UI3: util function for login

--- user-interface/src/utils/common.js
+++ user-interface/src/utils/common.js
@@ -1,7 +1,20 @@
+/**
+ * Shared utilities.
+ */
+
 export const isValidEmail = (email) => {
   return String(email)
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
+};
+
+/**
+ * Add timestamp to the message.
+ */
+export const consoleWTime = (message) => {
+  const d = new Date();
+  const dFormat = `[${d.getFullYear()}-${d.getMonth()}-${d.getDay()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
+  return `${dFormat} - ${message}`;
 };
